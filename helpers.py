0f7571937bfd6b4e96f419ac3343b5058a0b2ba1
julian <j.fiedler2002@outlook.com>
2023-12-13 22:05:57 +0100

    Implemented helper methods

---
+++ helpers.py
@@ -0,0 +1,27 @@
+def get_non_empty_input(min_length, prompt=''):
+    while True:
+        user_input = input(prompt)
+        if len(user_input) >= min_length:
+            return user_input
+        else:
+            print(f"Eingabe zu kurz. Es sind mindestens {min_length} Zeichen erforderlich.")
+
+
+def build_multiline_input_string(prompt_end='', prompt=''):
+    lines = []
+    while True:
+        line = input(prompt)
+        if line.strip() == prompt_end:
+            break
+        lines.append(line)
+    return '\n'.join(lines)
+
+
+def build_multiline_input_list(prompt_end='', prompt=''):
+    lines = []
+    while True:
+        line = input(prompt)
+        if line.strip() == prompt_end:
+            break
+        lines.append(line)
+    return lines
